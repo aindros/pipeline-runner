fdc8c69315519a7b4f5bf722c0b125d310bc1743
Alessandro Iezzi <a.iezzi@protonmail.com>
2026-02-12 23:51:15 +0100

    Manage the `variables` section

--- lib/pipeline.rb
+++ lib/pipeline.rb
@@ -5,11 +5,45 @@ require_relative 'job'
 class Pipeline
 	attr_accessor :jobs
 
+	private
+
+	def print_variables
+		if @variables.length == 0 then
+			return
+		end
+
+		@variables.each do |key, value|
+			puts "#{key}=\"#{value}\""
+		end
+
+		# Adding an empty line for next shell instructions
+		puts
+	end
+
+	def put_variables(variables)
+		variables.each do |key, value|
+			@variables[key] = value
+		end
+	end
+
+	def add_job(name, value)
+		job = Job.new
+		@jobs << job
+
+		job.name        = name
+		job.stage       = value["stage"]
+		job.script      = value["script"]
+		job.environment = value["environment"]
+	end
+
+	public
+
 	def initialize(filename = '.gitlab-ci.yml')
 		# @  -> Variabili di istanza
 		# @@ -> Variabili di classe
 		# $  -> Variabili globali
 		@filename = filename
+		@variables = {}
 	end
 
 	def parse
@@ -22,14 +56,18 @@ class Pipeline
 
 		@jobs = []
 		config.each do |key, value|
-			job = Job.new
-			@jobs << job
-
-			job.name        = key
-			job.stage       = config["#{key}"]["stage"]
-			job.script      = config["#{key}"]["script"]
-			job.environment = config["#{key}"]["environment"]
+			next if key == "stages" || key.start_with?(".")
+			case key
+			when "variables"
+				put_variables(value)
+			else
+				add_job(key, value)
+			end
 		end
+	end
+
+	def to_shell
+		print_variables
 
 		@jobs.each do |job|
 			job.toShell
