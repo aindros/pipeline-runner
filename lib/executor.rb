d316c78f5df1944595d371be853c1ced4f346021
Alessandro Iezzi <a.iezzi@protonmail.com>
2026-02-15 12:37:51 +0100

    Add the method to execute the runner inside a container

--- lib/executor.rb
+++ lib/executor.rb
@@ -1,8 +1,18 @@
 class Executor
+	private
+
+	def execute_as_container()
+	end
+
+	public
+
 	def initialize(config)
 		@type = config["executor"]
 	end
 
 	def execute()
+		case @type
+		when "podman"; execute_as_container();
+		end
 	end
 end
