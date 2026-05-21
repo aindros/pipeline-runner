f16e72ad02a47d80c6a0693a714060d3c8059ec1
Alessandro Iezzi <aiezzi@alessandroiezzi.it>
2026-05-21 16:42:03 +0200

    Parametrize the name of this project

--- lib/config.rb
+++ lib/config.rb
@@ -8,6 +8,8 @@ class Config
 
 	private
 
+	@@app_name = 'pipeline-runner'
+
 	def add_runner(config)
 		runner = Runner.new(config)
 		@runners[runner.name] = runner
@@ -39,14 +41,14 @@ class Config
 	def initialize()
 		@runners = {}
 		@config = "config.toml"
-		context_config = ".pipeline-parser/#{@config}"
-		home_config = "~/.config/pipeline-parser/#{@config}"
-		sys_config = "/usr/local/etc/pipeline-parser/#{@filename}"
+		context_config = ".#{@@app_name}/#{@config}"
+		home_config = "~/.config/#{@@app_name}/#{@config}"
+		sys_config = "/usr/local/etc/#{@@app_name}/#{@filename}"
 
 		# Order to check:
-		#   1. Context directory, where is executed pipeline-parser, so: .pipeline-parser/config.toml;
-		#   2. Home directory (~/.config/pipeline-parser/config.toml)
-		#   3. System directory (/usr/local/etc/pipeline-parser)
+		#   1. Context directory is where is executed pipeline-runner, so: .pipeline-runner/config.toml;
+		#   2. Home directory (~/.config/pipeline-runner/config.toml)
+		#   3. System directory (/usr/local/etc/pipeline-runner)
 		if File.exist?(context_config)
 			@config = context_config
 		elsif File.exist?(home_config)
