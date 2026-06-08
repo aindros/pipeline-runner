d1c8436987a7bd16bbef5821e1a4431b152fb8ff
Alessandro Iezzi <aiezzi@alessandroiezzi.it>
2026-06-08 16:44:29 +0200

    Add test for empty script section

--- test/test_gitlab_pipeline.rb
+++ test/test_gitlab_pipeline.rb
@@ -11,4 +11,10 @@ class TestGitLabPipeline < Minitest::Test
 		pipeline.parse()
 		pipeline.run()
 	end
+
+	def test_empty_script
+		pipeline = PipelineFactory::create("test/resources/gitlab-ci-empty-script.yml", PipelineType.by_name('gitlab'))
+		pipeline.parse()
+		pipeline.run()
+	end
 end
